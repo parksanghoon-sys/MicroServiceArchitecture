36a2057c3dc250e18b04300629d67c217780adec
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-04-08 23:07:51 +0900

    style : 주석추가

--- src/shared-libs/ECommerce.Shared/Infrastructure/RabbitMq/RabbitMqStartupExtensions.cs
+++ src/shared-libs/ECommerce.Shared/Infrastructure/RabbitMq/RabbitMqStartupExtensions.cs
@@ -6,6 +6,13 @@ namespace ECommerce.Shared.Infrastructure.RabbitMq;
 
 public static class RabbitMqStartupExtensions
 {
+    /// <summary>
+    /// IRabbitMqConnection 인터페이스를 DI 컨테이너에 등록합니다.
+    /// 싱글톤으로 Connection을 관리
+    /// </summary>
+    /// <param name="services">Service 등록을 위함</param>
+    /// <param name="configuration">RabbitMqOptions 을 Bind 하기 위함</param>
+    /// <returns></returns>
     public static IServiceCollection AddRabbitMqEventBus(this IServiceCollection services, IConfigurationManager configuration)
     {
         var rabbitMqOptions = new RabbitMqOptions();
@@ -15,6 +22,11 @@ public static class RabbitMqStartupExtensions
 
         return services;
     }
+    /// <summary>
+    /// 모든 마이크로 서비스가 이벤트를 게시하는 것은 아니므로 게시자만 호출해야 하는 새 메서드
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddRabbitMqEventPublisher(this IServiceCollection services)
     {
         services.AddScoped<IEventBus, RabbitMqEventBus>();
