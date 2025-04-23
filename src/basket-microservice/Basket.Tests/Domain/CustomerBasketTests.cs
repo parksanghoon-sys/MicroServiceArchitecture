366a0d2bdc07d01bf0cd1609d7d0906e022b91b6
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2025-04-23 15:44:17 +0900

    test : Basket test : 테스트 프로젝트 구현
    1. NSubstitute을 이용하여 Mock 작업
    2. 원할하게 사용하기 위한 DynamicProxyGenAssembly2 등록 Basket Service프로젝트
    
    * 추후 구현사항 : NAS 원격으로 Docker Compose 시 정상작동하는것

---
+++ src/basket-microservice/Basket.Tests/Domain/CustomerBasketTests.cs
@@ -0,0 +1,69 @@
+﻿using Basket.Service.Models;
+
+namespace Basket.Tests.Domain;
+
+public class CustomerBasketTestsDomainModel
+{
+
+    [Fact]
+    public void GivenAnEmptyCustomerBasket_WhenCallingAddBasketProducet_ThenProductAddedToBasket()
+    {
+        // Arrange
+        var product = new BasketProduct("1", "Test Name", 9.99M);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+
+        // Act
+        customerBasket.AddBasketProduct(product);
+
+        Assert.Contains(product, customerBasket.Products);
+    }
+    [Fact]
+    public void GivenCustomerBasketWithProduct_WhenCallingAddBasketProductWithExistingProduct_ThenBasketUpdated()
+    {
+        // Arrange
+        var product = new BasketProduct("1", "Test Name", 9.99M);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+        customerBasket.AddBasketProduct(product);
+
+        var updatedProduct = product with
+        {
+            Quantity = 2
+        };
+
+        // Act
+        customerBasket.AddBasketProduct(updatedProduct);
+
+        // Assert
+        Assert.Contains(updatedProduct, customerBasket.Products);
+        Assert.Equal(updatedProduct.Quantity, customerBasket.Products.First().Quantity);
+    }
+    [Fact]
+    public void GivenCustomerBasketWithProduct_WhenCallingRemoveBasketProduct_ThenProductSuccessfullyRemoved()
+    {
+        // Arrange
+        var product = new BasketProduct("1", "Test Name", 9.99M);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+        customerBasket.AddBasketProduct(product);
+
+        // Act
+        customerBasket.RemoveBasketProduct("1");
+
+        // Assert
+        Assert.Empty(customerBasket.Products);
+    }
+
+    [Fact]
+    public void GivenCustomerBasket_WhenAddingProduct_ThenBasketTotalCalculatedCorrectly()
+    {
+        // Arrange
+        const decimal basketTotal = 19.98M;
+
+        // Act
+        var product = new BasketProduct("1", "Test Name", 9.99M, 2);
+        var customerBasket = new CustomerBasket { CustomerId = "1" };
+        customerBasket.AddBasketProduct(product);
+
+        // Assert
+        Assert.Equal(basketTotal, customerBasket.BasketTotal);
+    }
+}
---
+++ src/basket-microservice/Basket.Tests/Endpoints/BasketApiEndpointsTests.cs
@@ -0,0 +1,125 @@
+﻿using Basket.Service.ApiModels;
+using Basket.Service.Endpoints;
+using Basket.Service.Infrastructure.Data;
+using Basket.Service.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Caching.Distributed;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Tests.Endpoints
+{
+    public class BasketApiEndpointsTests
+    {
+        private readonly IBasketStore _basketStore = Substitute.For<IBasketStore>();
+        private readonly IDistributedCache _cache = Substitute.For<IDistributedCache>();
+
+        [Fact]
+        public async Task GivenExistingBasket_WhenCallingGetBasket_ThenReturnsBasket()
+        {
+            const string customerId = "1";
+            var customerBasket = new CustomerBasket { CustomerId = customerId };
+
+            _basketStore.GetBasketByCustomerId(customerId)
+                .Returns(customerBasket);
+
+            // Act
+            var result = await BasketApiEndpoints.GetBasket(_basketStore, customerId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(customerId, result.CustomerId);
+        }
+        [Fact]
+        public async Task GivenNewBasketRequest_WhenCallingCreateBasket_ThenReturnsCreatedResult()
+        {
+            // Arrange
+            const string customerId = "1";
+            const string productId = "1";
+            var createBasketRequest = new CreateBasketRequest(productId, "Test Name");
+
+            _cache.GetAsync(productId)
+                .Returns(Encoding.UTF8.GetBytes("1.00"));
+
+            // Act
+            var result = await BasketApiEndpoints.CreateBasket(_basketStore, _cache, customerId, createBasketRequest);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var createdResult = (Created)result;
+            Assert.NotNull(createdResult);
+        }
+
+        [Fact]
+        public async Task GivenExistingBasket_WhenCallingAddBasketProduct_ThenReturnsNoContentResult()
+        {
+            // Arrange
+            const string customerId = "1";
+            var customerBasket = new CustomerBasket { CustomerId = customerId };
+
+            _basketStore.GetBasketByCustomerId(customerId).Returns(customerBasket);
+
+            const string productId = "1";
+            const int quantity = 2;
+            _cache.GetAsync(productId)
+                .Returns(Encoding.UTF8.GetBytes("1.00"));
+
+            var addBasketProductRequest = new AddBasketProductRequest(productId, "Test Name", quantity);
+
+            // Act
+            var result = await BasketApiEndpoints.AddBasketProduct(_basketStore, _cache, customerId, addBasketProductRequest);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var noContentResult = (NoContent)result;
+            Assert.NotNull(noContentResult);
+        }
+
+        [Fact]
+        public async Task GivenExistingBasketWithProducts_WhenCallingDeleteBasketProduct_ThenReturnsNoContentResult()
+        {
+            // Arrange
+            const string customerId = "1";
+            const string productId = "1";
+            var customerBasket = new CustomerBasket { CustomerId = customerId };
+
+            customerBasket.AddBasketProduct(new BasketProduct(productId, "Test Name", 9.99m));
+
+            _basketStore.GetBasketByCustomerId(customerId).Returns(customerBasket);
+
+            // Act
+            var result = await BasketApiEndpoints.DeleteBasketProduct(_basketStore, customerId, productId);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var noContentResult = (NoContent)result;
+            Assert.NotNull(noContentResult);
+        }
+
+        [Fact]
+        public async Task GivenExistingBasket_WhenCallingDeleteBasket_ThenReturnsNoContentResult()
+        {
+            // Arrange
+            const string customerId = "1";
+            var customerBasket = new CustomerBasket { CustomerId = customerId };
+
+            _basketStore.GetBasketByCustomerId(customerId).Returns(customerBasket);
+
+            // Act
+            var result = await BasketApiEndpoints.DeleteBasket(_basketStore, customerId);
+
+            // Assert
+            Assert.NotNull(result);
+
+            var noContentResult = (NoContent)result;
+            Assert.NotNull(noContentResult);
+        }
+    }
+}
