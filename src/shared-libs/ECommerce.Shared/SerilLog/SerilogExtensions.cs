39c4b2d38300b99dcf77951723686a7c772ae805
DESKTOP-L8PQMEN\User <power93266@naver.com>
2025-06-11 22:37:26 +0900

    feat : 인증 서비스 구축 (#3)
    1. 공통 라이브러리에서 Serilog 로깅 시스템 구축

---
+++ src/shared-libs/ECommerce.Shared/SerilLog/SerilogExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+
+namespace ECommerce.Shared.Log
+{
+    public static class SerilogExtensions
+    {
+        public static void AddSerilogLog(this WebApplicationBuilder webApplication)
+        {
+            Serilog.Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(webApplication.Configuration)
+                .WriteTo.Console()
+                .WriteTo.File(
+                    path: "logs/app-.log",
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 31,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
+                .CreateLogger();
+
+            webApplication.Host.UseSerilog(Serilog.Log.Logger);
+
+        }
+    }
+}
